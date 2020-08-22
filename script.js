0868e82dbb9997e720e021809e0c1bcde14ccae7
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-08-21 19:09:22 -0700

    Added a fallback photo attribute in case of 404 error, added functionality to edit and delete buttons

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ document.getElementById("submit_btn").addEventListener("click", () => {
 
   let card = createCard(dest, loc, pic, desc);
   document.getElementById("destinations_container").appendChild(card);
+  document.getElementById("destination").value = "";
+  document.getElementById("location").value = "";
+  document.getElementById("photo").value = "";
+  document.getElementById("description").value = "";
 });
 
 function createCard(destination, location, photo, description) {
@@ -16,7 +20,18 @@ function createCard(destination, location, photo, description) {
   // create card img element and set URL from Photo, append it to card
   let cardPic = document.createElement("img");
   cardPic.setAttribute("class", "card-img-top");
-  cardPic.setAttribute("src", photo);
+  if (photo.length == 0) {
+    cardPic.setAttribute(
+      "src",
+      "https://i2.wp.com/thepointsguy.com/wp-content/uploads/2020/04/GettyImages-1077097080.jpg?resize=2327%2C543px&ssl=1"
+    );
+  } else {
+    cardPic.setAttribute("src", photo);
+  }
+  cardPic.setAttribute(
+    "onerror",
+    "this.onerror=null;this.src='https://i2.wp.com/thepointsguy.com/wp-content/uploads/2020/04/GettyImages-1077097080.jpg?resize=2327%2C543px&ssl=1';"
+  );
   card.appendChild(cardPic);
 
   // create card body element
@@ -31,8 +46,8 @@ function createCard(destination, location, photo, description) {
 
   // create card subtitle and set it to location text, append it to card body
   let cardSubtitle = document.createElement("h6");
-  cardTitle.setAttribute("class", "card-subtitle");
-  cardTitle.innerHTML = location;
+  cardSubtitle.setAttribute("class", "card-subtitle");
+  cardSubtitle.innerHTML = location;
   cardBody.appendChild(cardSubtitle);
 
   // create card text and set it to description text, append it to card body
@@ -49,12 +64,14 @@ function createCard(destination, location, photo, description) {
   let editBtn = document.createElement("button");
   editBtn.setAttribute("class", "btn btn-secondary");
   editBtn.innerHTML = "Edit";
+  editBtn.addEventListener("click", editDestination);
   btnField.appendChild(editBtn);
 
   // create delete button and append it to button container
   let deleteBtn = document.createElement("button");
   deleteBtn.setAttribute("class", "btn btn-warning");
   deleteBtn.innerHTML = "Delete";
+  deleteBtn.addEventListener("click", deleteDestination);
   btnField.appendChild(deleteBtn);
 
   // append completed button container to card body
@@ -64,4 +81,38 @@ function createCard(destination, location, photo, description) {
   card.appendChild(cardBody);
 
   return card;
+}
+
+function editDestination(event) {
+  let card = event.target.parentElement.parentElement.parentElement;
+  let photoUrl = card.children[0];
+  let cardBody = card.children[1];
+  let destination = cardBody.children[0];
+  let location = cardBody.children[1];
+  let description = cardBody.children[2];
+
+  let newDestination = prompt("Enter the new destination:");
+  if (newDestination.length > 0) {
+    destination.innerHTML = newDestination;
+  }
+
+  let newLocation = prompt("Enter the new location:");
+  if (newLocation.length > 0) {
+    location.innerHTML = newLocation;
+  }
+
+  let newPhotoUrl = prompt("Enter the URL of a new photo:");
+  if (newPhotoUrl.length > 0) {
+    photoUrl.setAttribute("src", newPhotoUrl);
+  }
+
+  let newDescription = prompt("Enter the new description:");
+  if (newDescription.length > 0) {
+    description.innerHTML = newDescription;
+  }
+}
+
+function deleteDestination(event) {
+  let card = event.target.parentElement.parentElement.parentElement;
+  card.remove();
 }
